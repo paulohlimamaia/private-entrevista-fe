429f0d5178006d636f639e151e7ef699fdd5d2bd
Paulo Henrique <p.henrique32@live.com>
2020-08-13 16:23:24 -0300

    about section

---
+++ src/yourprojectname/scripts/components/about/about.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="about">
+      <div class="about-content">
+        <div class="about-left">
+            <h1 class="about-title">About the show</h1>
+            <hr class="about-hr" />
+            <h4 class="about-text">So they stick around, kicking out feedback and I entertain it as if I need that I had a talk with my uncle and he agreed that my privacy about the only thing I need back as if I need that I had a talk with my uncle and he agreed that my privacy about the only thing.</h4>
+        </div>
+        <div class="about-right">
+            <img class="about-image" src="/styles/images/image-dev.png" alt="dev" />
+        </div>
+      </div>
+      <img class="about-image-mobile" src="/styles/images/image-dev.png" alt="dev" />
+
+      <div class="background-text-about">About<br>the<br>show</div>
+      <img class="blob-about" src="/styles/images/blob1.png" alt="blob" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "about"
+};
+</script>
--- src/yourprojectname/scripts/entry.js
+++ src/yourprojectname/scripts/entry.js
@@ -2,11 +2,21 @@ import Vue from "vue";
 
 // Import Components
 import HireMePlease from "./components/hire-me-please/hire-me-please.vue";
-import ThePodcastPage from "./components/the-podcast-page/the-podcast-page.vue";
+import Hero from "./components/hero/hero.vue";
+import Episodes from "./components/episodes/episodes.vue";
+import About from "./components/about/about.vue";
+import Next from "./components/next/next.vue";
+import Footer from "./components/footer/footer.vue";
+
+
 
 // Vue Declaration for Components
 Vue.component(HireMePlease.name, HireMePlease);
-Vue.component(ThePodcastPage.name, ThePodcastPage);
+Vue.component(Hero.name, Hero);
+Vue.component(Episodes.name, Episodes);
+Vue.component(About.name, About);
+Vue.component(Next.name, Next);
+Vue.component(Footer.name, Footer);
 
 // Import Directives
 import { backgroundDirective, ellipsis } from "./directives";
