ccd3a06f11f8456de632de8009cbf367af134104
Paulo Henrique <p.henrique32@live.com>
2020-08-13 16:28:18 -0300

    next section

---
+++ src/yourprojectname/scripts/components/next/next.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="next">
+      <div class="next-content">
+        <div class="next-left">
+            <img class="next-image" src="/styles/images/image-cto.png" alt="dev" />
+        </div>
+        <div class="next-right">
+            <h1 class="next-title">Coming next</h1>
+            <h4 class="next-text">Body placeholder for text paragraph. A paragraph is a self-contained unit of text dealing with a particular point or idea is a self-contained unit of text dealing with a particular point or idea.</h4>
+        </div>
+      </div>
+
+      <h1 class="next-image-title">Danny<br>Baker</h1>
+      <h1 class="next-image-subtitle">Fake Company CTO</h1>
+      <hr class="next-hr" />
+      <div class="background-text-next">Next</div>
+      <div class="stripes-next-1"></div>
+      <div class="stripes-next-2"></div>
+      <img class="blob-next" src="/styles/images/blob2.png" alt="blob" />
+      <img class="blob-next-2" src="/styles/images/blob3.png" alt="blob" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "next"
+};
+</script>
